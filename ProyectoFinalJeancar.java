6d8d2a2abd5a44a63a343ce757fff849a5a97e60
Jeanca-zzz <jeancarlos1929292927@gmail.com>
2025-12-15 21:32:16 -0600

    Add files via upload
    el día de hoy se crea el proyecto 2 Sistema de Biblioteca Personal  donde al método mai le definimos el set visible true para que en un futuro nos inicialise el jframe además de ello se crean 7 clases mas las cuales por el momento se encuentran vacías aparte del jframe y el mai dando un total de 9 clases  las mismas serán necesarias para la elaboración del proyecto

---
+++ ProyectoFinalJeancar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinaljeancar;
+
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author JNC
+ */
+public class ProyectoFinalJeancar {
+
+
+public class Main {
+  
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new FmrBiblioteca().setVisible(true);
+        });
+    }
+}
+}
