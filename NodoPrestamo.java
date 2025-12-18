7f6e38699595913780723dcdc74531a908aca815
Jeanca-zzz <jeancarlos1929292927@gmail.com>
2025-12-17 21:15:57 -0600

    Add files via upload
    el dia de hoy no se logra avansar mucho por lo que solo se avansa la clase  nodo prestamo el cual muestra errores que el dia de mañana seran ebacuadas a la hora de finalizar las mallorias de clases pendiente 2 clases faltante y solo nos quedaria proseder a configurar el jframe el cual sea llamativo al usuario

---
+++ NodoPrestamo.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectofinaljeancar;
+
+/**
+ *
+ * @author JNC
+ */
+public class NodoPrestamo {
+
+private NodoPrestamo frente;
+    private NodoPrestamo fin;
+
+    public boolean estaVacia() {
+        return frente == null;
+    }
+
+    public void encolar(Prestamo p) {
+        NodoPrestamo nuevo = new NodoPrestamo(p);
+        if (estaVacia()) {
+            frente = fin = nuevo;
+        } else {
+            fin.sig = nuevo;
+            fin = nuevo;
+        }
+    }
+
+    public Prestamo desencolar() {
+        if (estaVacia()) return null;
+        Prestamo p = frente.dato;
+        frente = frente.sig;
+        if (frente == null) fin = null;
+        return p;
+    }
+
+    public Prestamo verFrente() {
+        return estaVacia() ? null : frente.dato;
+    }
+
+    public NodoPrestamo getFrente() { return frente; }
+    public void setFrente(NodoPrestamo frente) { this.frente = frente; }
+    public NodoPrestamo getFin() { return fin; }
+    public void setFin(NodoPrestamo fin) { this.fin = fin; }
+}
+
+
+
+    
+
